031ec8bc916a3a42a1ed6e24d1962d4da5b6a208
alexandercyu <alexanderc.yu@utoronto.ca>
2025-07-10 15:41:59 -0400

    Update index.ts
    - Add code cell with pre-filled code
    - run the cell (currently doens't seem to be running but the return on the function runCells implies it is running)

--- jupyterlab_marble_extension/src/index.ts
+++ jupyterlab_marble_extension/src/index.ts
@@ -3,7 +3,11 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import {
+    INotebookTracker,
+    NotebookActions
+} from '@jupyterlab/notebook'
+
 
 /**
  * Initialization data for the jupyterlab-marble-extension extension.
@@ -12,8 +16,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-marble-extension:plugin',
   description: 'A JupyterLab extension that gets the session of the marble node',
   autoStart: true,
-  optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  requires: [INotebookTracker],
+  optional: [],
+  activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
     console.log('JupyterLab extension jupyterlab-marble-extension is activated!');
 
 
@@ -21,20 +26,58 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     const command = 'jupyterlab-marble-extension:command';
 
-commands.addCommand(command, {
-  label: 'Execute jlab-examples:command Command',
-      caption: 'Execute jlab-examples:command Command',
-execute: (args: any) => {
-      const current = notebookTracker.currentWidget;
-    const notebook = current.content;
-    NotebookActions.insertBelow(notebook);
-    const activeCell = notebook.activeCell;
+    commands.addCommand(command, {
+        label: 'Get Marble Session',
+      caption: 'Get Marble Session',
+        execute: async (args: any) => {
+        const current = notebookTracker.currentWidget;
+        const notebook = current!.content;
+        NotebookActions.insertAbove(notebook);
 
-        const orig = args['origin'];
-        console.log(`jlab-examples:command has been called from ${orig}.`);
-        if (orig !== 'init') {
-          window.alert(`jlab-examples:command has been called from ${orig}.`);
-        }
+
+        const activeCell = notebook.activeCell;
+
+
+        activeCell!.model.sharedModel.setSource('import sys' +
+            '\n!{sys.executable} -m pip install marble_client' +
+            '\nfrom marble_client import MarbleClient' +
+            '\nclient = MarbleClient()' +
+            '\nprint(client.this_node)' +
+            '\nsession = requests.Session()' +
+            '\nclientSession = this_session(session)' +
+            '\nprint(client)')
+
+
+
+
+        const cellReturn = await NotebookActions.runCells(notebook, [activeCell!]);
+        console.log("activeCell")
+        console.log(activeCell)
+        console.log("cellReturn")
+        console.log(cellReturn)
+
+          NotebookActions.runAndAdvance(notebook)
+          NotebookActions.selectLastRunCell(notebook);
+
+          NotebookActions.showAllCode(notebook)
+          //NotebookActions.toggleAllLineNumbers(notebook);
+
+
+            //const cellOutput = activeCell.runCell();
+        /*"cells" : [
+    {
+      "cell_type" : "code",
+      "execution_count": null,
+      "metadata" : {},
+      "source" : "[some multi-line code]",
+      "outputs": [],
+    }
+  ]*/
+
+
+
+
+
       }
 
 });
@@ -42,21 +85,12 @@ execute: (args: any) => {
     // Call the command execution
     commands.execute(command, { origin: 'init' }).catch(reason => {
 console.error(
-        `An error occurred during the execution of jlab-examples:command.\n${reason}`
+        `An error occurred during the execution of jupyterlab-marble-extension:command.\n${reason}`
       );
 
     });
 
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('jupyterlab-marble-extension settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for jupyterlab-marble-extension.', reason);
-        });
-    }
+
 
   }
 };
