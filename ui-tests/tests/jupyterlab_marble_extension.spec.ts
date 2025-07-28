4ded0231e1fb7c4c6b92c113a0c0fa9228378e2f
alexandercyu <alexanderc.yu@utoronto.ca>
2025-07-28 13:39:48 -0400

    Update jupyterlab_marble_extension.spec.ts
    Added tests (tests in progress)

--- ui-tests/tests/jupyterlab_marble_extension.spec.ts
+++ ui-tests/tests/jupyterlab_marble_extension.spec.ts
@@ -1,5 +1,5 @@
-import { galata, expect, test } from '@jupyterlab/galata';
-
+import { test } from '@jupyterlab/galata';
+import { expect } from '@playwright/test';
 /**
  * Don't load JupyterLab webpage before running the tests.
  * This is required to ensure we capture all log messages.
@@ -16,12 +16,45 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension jupyterlab-marble-extension is activated!')
+    logs.filter(
+      s =>
+        s === 'JupyterLab extension jupyterlab-marble-extension is activated!'
+    )
   ).toHaveLength(1);
 });
 
-test('should add a code cell at the top of the current notebook', async() =>{
-  await galata.notebook.addCell('code')
-  expect(await galata.notebook.getCellType(0).toBe('code'));
+/*
+test('marble-login command should be called', async() =>{
 
 })
+*/
+/*
+test('should add a code cell at the top of the current notebook', async() =>{
+  //await page.notebook.addCell('code')
+  expect(await getCellType(0)).toBe('code');
+
+})
+*/
+test('new notebook created', async ({ page, tmpPath }) => {
+  const newNotebook = 'create_test.ipynb';
+  await page.notebook.createNew(newNotebook);
+
+  expect(
+    await page.waitForSelector(`[role="main"] >> text=${newNotebook}`)
+  ).toBeTruthy();
+
+  expect(await page.contents.fileExists(`${tmpPath}/${newNotebook}`)).toEqual(
+    true
+  );
+
+  //expect(await getToolbarItemByIndex(10))
+});
+
+/*
+test('the added code cell should contain code/strings of code', async({page}) =>{
+  //const notebook = "Untitled.ipynb";
+  expect(await page.notebook.getCellIndex(0)).expect.anything()
+})
+
+
+*/
